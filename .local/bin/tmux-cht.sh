1a4617644df7842f3ff7fc3a9eb91e6645fabf69
Marian Minar <majamin@gmail.com>
2023-01-21 08:01:13 -0800

    clean up tmux config

--- .local/bin/tmux-cht.sh
+++ .local/bin/tmux-cht.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
-
-languages="$HOME/.config/tmux/tmux-cht-languages"
-command="$HOME/.config/tmux/tmux-cht-command"
-
-selected=`cat "$languages" "$command" | fzf`
-if [[ -z $selected ]]; then
-    exit 0
-fi
-
 read -p "Enter Query: " query
-
-if grep -qs "$selected" "$languages"; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
-else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
-fi
+tmux neww -n "cht-sh-${query%% *}" bash -c "cht.sh $query | less"
