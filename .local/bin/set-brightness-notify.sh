a870ac657be17bb06e232dc73325b4340a1f032a
Marian Minar <majamin@gmail.com>
2023-04-21 21:52:09 -0700

    brightness works

--- .local/bin/set-brightness-notify.sh
+++ .local/bin/set-brightness-notify.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-brightnessctl set $1
-ticks=$(printf %.f $(echo "$(brightnessctl g)/$(brightnessctl m)*22" | bc -l))
-bar=$(seq -s "─" 0 $ticks | sed 's/[0-9]//g')
+# Requires user to be in video group and:
+# -- /etc/udev/rules.d/backlight.rules --
+# ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
+
+updown="$1"
+
+CURR_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/brightness)
+MAX_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+
+STEP=$( echo "$MAX_BRIGHTNESS" / 12 | bc )
+TARGET_BRIGHTNESS=$( echo "$CURR_BRIGHTNESS" " + " "$updown" " * " "$STEP" | bc )
+
+[ "$TARGET_BRIGHTNESS" -gt "$MAX_BRIGHTNESS" ] && TARGET_BRIGHTNESS="$MAX_BRIGHTNESS"
+tick=$( echo "$TARGET_BRIGHTNESS / $MAX_BRIGHTNESS * 12" | bc -l )
+
+echo $TARGET_BRIGHTNESS > /sys/class/backlight/intel_backlight/brightness
+bar=$(seq -s "──" 0 "$tick" | sed 's/[0-9]//g')
 notify-send -t 1500 -r 1010 -h string:bgcolor:#111111 -h string:fgcolor:#eeeeee "益 $bar"
