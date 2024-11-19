03967ec138d4ab5430b09b0a19c430419bcb2912
Marian Minar <majamin@gmail.com>
2024-11-18 18:51:19 -0800

    update imaging.sh

--- .local/bin/imaging.sh
+++ .local/bin/imaging.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
 # required first argument is the path to the image directory
-IMAGE_DIR=$1
+src_dir=$1
+out_dir="images"
 
 # Check if the directory exists
-if [ ! -d "$IMAGE_DIR" ]; then
-    echo "Directory does not exist"
+if [ ! -d "$src_dir" ]; then
+    printf "Directory '%s' does not exist" "${src_dir}"
     exit 1
 fi
 
 # Find all files in the directory using fd-find
 # Pipe the output to nsxiv for image selection
-mapfile -t selection < <(fd -e jpg -e png -e gif -e bmp -e tiff -e webp -e svg -e jpeg -e heic . "$IMAGE_DIR" -0 | xargs -0 ls -t | nsxiv -troi)
+mapfile -t allfiles < <(fd -e jpg -e png -e gif -e bmp -e tiff -e webp -e svg -e jpeg -e heic . "$src_dir" -0 | xargs -0 ls -t | nsxiv -troi)
 
 # Check if the user selected any images
-if [ ${#selection[@]} -eq 0 ]; then
+if [ ${#allfiles[@]} -eq 0 ]; then
     echo "No images selected"
     exit 1
 fi
 
 # Create the images subdirectory if it doesn't exist
-mkdir -p images 2>/dev/null
+mkdir -p "$out_dir" 2>/dev/null
 
 # Copy the selected images to the images subdirectory with spaces replaced by hyphens
 declare -a new_name
-for image in "${selection[@]}"; do
+for image in "${allfiles[@]}"; do
     # Get the basename of the image and replace spaces with hyphens
     # set new extension to png
     new_name=$(basename "$image" | tr ' ' '-' | sed 's/\(.*\)\..*/\1.png/')
 
     # convert image to png using image magick,
     # and copy the file to the images/ directory with the new name
-    convert "$image" -auto-orient -resize 1600x "images/$new_name"
+    magick "$image" -auto-orient -resize 1600x "${out_dir}/${new_name}"
 
     # Print the new name of the image
     echo "$new_name"
