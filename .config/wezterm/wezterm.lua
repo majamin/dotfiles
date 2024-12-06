79c40cf9295e11916a5c8722c6eec74d62c02f12
Marian Minar <majamin@gmail.com>
2024-12-05 21:22:08 -0800

    better light theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 
+-- check for set-light-theme in zsh directory
 local light = false
 local f = io.open(os.getenv("HOME") .. "/.config/zsh/set-light-theme", "r")
 if f ~= nil then
@@ -9,26 +10,21 @@ end
 
 local config = {
   audible_bell = "Disabled",
-  visual_bell = {
-    fade_in_function = "EaseIn",
-    fade_in_duration_ms = 150,
-    fade_out_function = "EaseOut",
-    fade_out_duration_ms = 150,
-  },
-  colors = {
-    visual_bell = "#202020",
-  },
-  color_scheme = (light and "PencilLight") or "Tokyo Night Storm",
+  color_scheme = (light and "AtomOneLight") or "Tokyo Night Storm",
   cursor_blink_ease_in = "Constant",
   cursor_blink_ease_out = "Constant",
   cursor_blink_rate = 680,
   enable_tab_bar = false,
-  -- window_background_opacity = 0.91,
   window_close_confirmation = "NeverPrompt",
-  font = wezterm.font(
-    "FiraCode Nerd Font",
-    { weight = "Medium", italic = false }
-  ),
+  -- window_background_opacity = 0.96,
+  font = wezterm.font_with_fallback({
+    {
+      family = "FiraCode Nerd Font", -- main font
+      weight = "Medium",
+      italic = false,
+    },
+    "Noto Color Emoji", -- fallback font
+  }),
   font_size = 14.0,
   bold_brightens_ansi_colors = false,
   freetype_load_target = "Light",
